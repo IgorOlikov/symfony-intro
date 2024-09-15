d2ca2e418309f154d32a851f105aa5fb9681d28b
IgorOlikov <i.olikov@mail.ru>
2024-09-16 00:53:05 +0500

    password reset

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -6,17 +6,22 @@ use App\Entity\Post;
 use App\Entity\User;
 use App\Form\PostType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, MailerInterface $mailer): Response
     {
         //$this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+
+
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
